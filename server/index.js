ee47f74c8c4399ec7b3f84c339cfb1b574b42076
MANASPASARKAR <impasarkarmanas@gmail.com>
2026-05-04 21:58:59 +0530

    added cors of vercel frontend

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const cors = require("cors")
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://your-vercel-app.vercel.app"
+    "https://devjrnl.vercel.app/"
   ],
   credentials: true
 }));
